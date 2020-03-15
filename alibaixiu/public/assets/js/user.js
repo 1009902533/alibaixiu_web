b25a857add69737bce1654f872f3498f18d5d112
xiapingping <a54960@qq.com>
2020-03-15 11:24:03 +0800

    头像上传功能

--- alibaixiu/public/assets/js/user.js
+++ alibaixiu/public/assets/js/user.js
@@ -12,3 +12,22 @@ function render(){
     var html = template('tpl',{data:userlist})
     $("#userlist").html(html)
 }
+//上传头像
+$("#avatar").on('change',function(){
+    var formdata = new FormData();
+    formdata.append('avatar',this.files[0]);
+    $.ajax({
+        type:'post',
+        url:'/upload',
+        data:formdata,
+        //不解析formData
+        processData:false,
+        //不添文件加类型
+        contentType:false,
+        success:function(res){
+            console.log(res)
+            $("#img").attr('src',res[0].avatar);
+            $("#imgurl").val(res[0].avatar)
+        }
+    });
+})
