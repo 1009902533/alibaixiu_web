005c06d3a18c4fee5bbfe2d430db06f7604f66d5
xiapingping <a54960@qq.com>
2020-03-18 11:12:02 +0800

    筛选分页功能优化

--- alibaixiu/public/assets/js/posts.js
+++ alibaixiu/public/assets/js/posts.js
@@ -1,9 +1,13 @@
-function wzlist(p){
+var fenlei = $("#allfenlei").val();
+var zhuangtai = $("#allchange").val();
+function render(cid,state,page = 1){
     $.ajax({
         type: "get",
         url: "/posts",
         data:{
-            page:p
+            category:cid,
+            state:state,
+            page:page
         },
         success: function (res) {
             var html = template('tpl',{data:res.records});
@@ -13,60 +17,23 @@ function wzlist(p){
         }
     });
 }
-function pages(m) {
-    wzlist(m)
+
+render(fenlei,zhuangtai);
+function pages(index) {
+    render(fenlei,zhuangtai,index);
 }
     
-wzlist(1);
 $.ajax({
     type:"get",
     url:"/categories",
     success:function(res){
         var html = template('alltem',{data:res})
-        $("#allfenlei").html(html);
+        $("#allfenlei").append(html);
     }
 })
 //分类查询
 $("#shaixuan").on('click',function(){
-    var fenlei = $("#allfenlei").val();
-    var zhuangtai = $("#allchange").val();
-    if(fenlei == 0){
-        $.ajax({
-            type: "get",
-            url: "/posts",
-            data:{
-                page:1
-            },
-            success: function (res) {
-                res = res.records;
-                shaixuan(res,zhuangtai)
-            }
-        });
-    } else {
-        $.ajax({
-            type:'get',
-            url:'/posts/category/' + fenlei,
-            success:function(res){
-                shaixuan(res,zhuangtai)
-            }
-        })
-    }
+    fenlei = $("#allfenlei").val();
+    zhuangtai = $("#allchange").val();
+    render(fenlei,zhuangtai);
 })
-function shaixuan(res,zhuangtai){
-    if(zhuangtai == 0) {
-        var newarr = res.filter((item)=>{
-            return item.state === 0
-        });
-        var html = template('tpl',{data:newarr});
-        $("#wzlist").html(html);
-    } else if(zhuangtai == 1) {
-        var newarr = res.filter((item)=>{
-            return item.state === 1
-        });
-        var html = template('tpl',{data:newarr});
-        $("#wzlist").html(html);
-    } else {
-        var html = template('tpl',{data:res});
-        $("#wzlist").html(html);
-    }
-}
