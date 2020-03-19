95c2355f10713895c7f19752774828c2e9a1fd8e
xiapingping <a54960@qq.com>
2020-03-20 00:00:34 +0800

    路由优化

--- alibaixiu/routes/actions/comment/findlist.js
+++ alibaixiu/routes/actions/comment/findlist.js
@@ -2,9 +2,9 @@
 const { Comment } = require('../../../model/Comment');
 
 module.exports = async (req, res) => {
-	// 查询用户信息
+	// 获取文章id
 	var id = req.query.id
-	// 查询用户信息
+	// 根据文章id查询文章的评论
 	const posts = await Comment.find({post:id}).populate('author', '-password')
 	// 响应
 	res.send(posts);
